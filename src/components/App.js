0ccf4e63d7163ee9793e89595df76c57df87237e
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-02-08 23:15:35 +0100

    :sparkles: Get Facebook events

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import Facebook from '../lib/Facebook';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        events: false,
+    }
+  }
+
+  componentDidMount() {
+    const facebook = new Facebook();
+
+    facebook.getEvents().then(events => {
+      this.setState({events: events});
+    });
+  }
+
   render() {
+    let eventsList = 'Loading...';
+
+    if (this.state.events) {
+      eventsList = this.state.events.map(item => {
+        console.log(item);
+        return <div key="{item.id}">{item.name}</div>
+      });
+    }
+
     return (
       <div className="app">
         <h1>CeeCee events</h1>
         <p>Upcomming events in the creative community of Enschede</p>
+
+        {eventsList}
       </div>
     );
   }
---
+++ src/lib/Facebook.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Facebook
+{
+  get(endpoint, params = {}) {
+    return axios.request({
+      method: 'get',
+      baseURL: 'https://graph.facebook.com/v2.12/',
+      url: endpoint,
+      params: Object.assign({'access_token': process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN}, params),
+    }).then(response => {
+      return response.data.data;
+    });
+  }
+
+  getEvents() {
+    return this.get('theceespot/events');
+  }
+}
