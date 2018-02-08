5f36f428c5ae7d9e74de9dd5730c3771bfe63c10
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-02-08 23:35:19 +0100

    :construction: Only show upcomming events

--- src/lib/Facebook.js
+++ src/lib/Facebook.js
@@ -14,6 +14,8 @@ export default class Facebook
   }
 
   getEvents() {
-    return this.get('theceespot/events');
+    return this.get('theceespot/events', {
+      time_filter: 'upcoming',
+    });
   }
 }
