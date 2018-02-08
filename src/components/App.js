effa701b4075e95443e437413d2f1dffee2c1ba5
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-02-08 21:01:00 +0100

    :pencil2:Title

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header">
-          <h1 className="App__title">CeeCee</h1>
-        </header>
+        <h1>CeeCee events</h1>
+        <p>The creative community of Enschede</p>
       </div>
     );
   }
