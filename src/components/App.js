61e426dff12668c80f867a525dc8755fc9a307a0
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-02-08 20:52:55 +0100

    :fire: Obsolete stuff

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1 className="App__title">CeeCee</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
